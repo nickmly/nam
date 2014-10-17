81d255350bdee6673ff7c2053824d4159eb78b76
ReallyAsseel <a.sidique@hotmail.com>
2014-10-16 22:41:55 -0400

    Animations Fixed, All is VELL

--- Assets/Scripts/ArmScriptThough.cs
+++ Assets/Scripts/ArmScriptThough.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class LegScript : MonoBehaviour {
+public class ArmScriptThough : MonoBehaviour {
 
 	public Movement movement;
 	public Animator anim;
 
 	// Use this for initialization
 	void Start () {
+	
 		movement.GetComponent<Movement>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	
 		anim.SetBool("isRunning", movement.isRunning);
 		anim.SetBool("HasJumped", movement.hasJumped);
+
 	}
 }
