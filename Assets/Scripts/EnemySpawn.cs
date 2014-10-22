1053dc5fb8e74551715c436ffa7bc3947b047e97
ReallyAsseel <a.sidique@hotmail.com>
2014-10-22 18:45:36 -0400

    Wave System

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour
+{
+	public GameObject enemy;
+	public Vector3 spawnValues;
+	public int enemyCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	
+	void Start ()
+	{
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < enemyCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3(68f,3f,1.57f);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (enemy, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+	
+}
